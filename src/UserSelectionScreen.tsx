d44cadca809da9e224db66cb6235028af8e44cc1
Abhishek9739 <abhi.rishi9739@gmail.com>
2023-12-08 16:19:32 +0530

    User selction UI update

--- src/UserSelectionScreen.tsx
+++ src/UserSelectionScreen.tsx
@@ -1,33 +1,78 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { View, StyleSheet, TouchableOpacity, ImageBackground, Text } from 'react-native';
 import { Button, Layout } from '@ui-kitten/components';
 import { useNavigation } from '@react-navigation/native';
+import * as Font from 'expo-font';
 
 interface UserSelectionScreenProps {}
 
 const UserSelectionScreen: React.FC<UserSelectionScreenProps> = () => {
   const navigation = useNavigation();
+  const fontLoadedRef = useRef(false);
+
+//Custom Font Addition
+const BlueStyle = require('./Fonts/Pixer.ttf');
+
+useEffect(() => {
+  async function loadFonts() {
+    await Font.loadAsync({
+      'my-font': BlueStyle,
+    });
+    fontLoadedRef.current = true;
+  }
+
+  loadFonts();
+}, []);
+
+// Removing Header
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, [navigation]);
   
   const handleUserSelection = (sender: string, recipient: string) => {
     // Navigate to AnotherScreen and pass the selected user as a parameter
     navigation.navigate('ChatScreen', { senderUser: sender, recipientUser: recipient });
+
+    
   };
 
   return (
+
     <Layout style={styles.container}>
-      <Button
-        style={styles.button}
-        onPress={() => handleUserSelection('Cyril', 'Afbi')}
-      >
-        You are Cyril
-      </Button>
-      <Button
-        style={styles.button}
-        onPress={() => handleUserSelection('Afbi', 'Cyril')}
-      >
-        You are Afbi
-      </Button>
+    <Text style={styles.text1}> Who Are You???</Text>
+      <TouchableOpacity style={styles.button1} onPress={() => handleUserSelection('Cyril', 'Afbi')}>
+        <ImageBackground source={require('./Media/C.jpeg')} style={styles.image}>
+          {/* Any additional content or text can be added here */}
+        </ImageBackground>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button2} onPress={() => handleUserSelection('Afbi', 'Cyril')}>
+        <ImageBackground source={require('./Media/A.jpeg')} style={styles.image}>
+          {/* Any additional content or text can be added here */}
+        </ImageBackground>
+      </TouchableOpacity>
     </Layout>
+    
+    
+    
+    
+    // <Layout style={styles.container}>
+    //   <Button
+    //     style={styles.button}
+    //     onPress={() => handleUserSelection('Cyril', 'Afbi')}
+    //   >
+        
+    //     You are Cyril
+    //   </Button>
+    //   <Button
+    //     style={styles.button}
+    //     onPress={() => handleUserSelection('Afbi', 'Cyril')}
+    //   >
+    //     You are Afbi
+    //   </Button>
+    
+    // </Layout>
   );
 };
 
@@ -38,9 +83,39 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 16,
   },
-  button: {
-    marginVertical: 8,
+
+  button1: {
+    height:150,
+    width:150,
+   marginVertical: 30,
+   overflow: 'hidden',
+   borderRadius: 20,
+   right:60
   },
+
+  button2: {
+    height:150,
+    width:150,
+   marginVertical: 30,
+   overflow: 'hidden',
+   borderRadius: 20,
+   left:60
+  },
+
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+
+  text1:{
+    fontFamily: 'my-font', 
+    color: '#FFFFFF',
+    fontSize:50,
+    height:100
+  }
 });
 
+
+
 export default UserSelectionScreen;
+
